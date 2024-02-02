735462787c3b952319afb3397e933ef8ace7c4d3
MaartenKnaepen <maarten.kn@gmail.com>
2024-02-02 11:06:15 +0100

    Final commit for basic version

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
-from src.scraper import WikipediaScraper, select_country
+from src.scraper import WikipediaScraper
 
 def main():
-    k = 0
-    while k != 6:
-        k = input('Welcome to LeaderScraper! For which countries would you like to see date about their leaders? \n'
-            'for Belgium, press 1 \n'
-            'for France, press 2 \n'
-            'for Morocco press 3 \n'
-            'for Russia press 4 \n'
-            'for the United States of America press 5 \n'
-            'for all countries press 6 \n'
-            'to close this program, press 7  ')
-        wikiscrap = WikipediaScraper()
-        if int(k) == 7:
+    """
+    The main function of the LeaderScraper program.
+
+    This function initializes a WikipediaScraper object and provides a user interface for
+    interacting with the scraping functionality to retrieve information about country leaders.
+
+    Usage:
+    - Run this script to execute the LeaderScraper program.
+    """
+    wikiscrap = WikipediaScraper()  # Initialize outside the loop
+
+    while True:
+        print('\nWelcome to LeaderScraper! \n'
+              'For available countries, press 1 \n'
+              'For info about leaders of all countries, press 2 \n'
+              'To close this program, press 3')
+
+        user_input = input('Enter your choice: ')
+
+        if int(user_input) == 1:
+            # Display available countries
+            print(f'Available countries are: {wikiscrap.get_countries()}')
+
+        elif int(user_input) == 2:
+            # Scraping and saving leaders' information
+            print('Go get a coffee. This will take a while.')
+            wikiscrap.get_leaders()
+            wikiscrap.save_data('json', 'leaders')
+            print('Data collection was successful! The output is saved in the file leaders.json')
+        elif int(user_input) == 3:
+            # Exit the program
             exit()
-        elif int(k) < 6:
-            cntry = select_country(k)
-            wikiscrap.get_leaders(cntry)
-        
-        wikiscrap.into_dataframe()
-        bio_flag = input('Do you want to include a short bio for each leader? yes or no.  ')
-        if bio_flag =='yes':
-            wikiscrap.append_wiki_bio()
-        filetype_flag = input('How do you want to save your file? excel, csv or json   ')
-        wikiscrap.save_data(filetype_flag)
+        else:
+            # Handle invalid input
+            print('Please enter a valid number')
 
 if __name__ == "__main__":
+    # Execute the main function if the script is run
     main()
--- src/scraper.py
+++ src/scraper.py
@@ -1,92 +1,153 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import pandas as pd
-from json import loads, dumps
+from json import dumps
 import json
-
-main_dict = {1:'be', 2:'fr',3:'ma', 4:'ru',5:'us'}
-   
-def select_country(key):
-    value = main_dict[int(key)]
-    return value
+import csv
 
 class WikipediaScraper:
+    """
+    A class for scraping and retrieving information about country leaders from Wikipedia.
+
+    Attributes:
+    - base_url: The base URL for the Wikipedia scraper.
+    - country_endpoint: The endpoint for obtaining information about available countries.
+    - leaders_endpoint: The endpoint for retrieving information about country leaders.
+    - cookies_endpoint: The endpoint for obtaining and refreshing cookies.
+    - leaders_data: A dictionary to store leaders' information.
+    - cookie: Cookies used for making requests to the scraper.
+    - df: Pandas DataFrame to store the leader information.
+    - leader_json: JSON representation of leader data.
+    """
+
     def __init__(self):
+        """
+        Initialize the WikipediaScraper object.
+        """
         self.base_url = 'https://country-leaders.onrender.com'
         self.country_endpoint = self.base_url + '/countries'
         self.leaders_endpoint = self.base_url + '/leaders'
         self.cookies_endpoint = self.base_url + '/cookie'
-        self.leaders_data = {}
         self.cookie = requests.get(self.cookies_endpoint).cookies
         self.df = None
         self.leader_json = None
-    
+
     def refresh_cookie(self):
+        """
+        Refresh the cookie used for making requests to the scraper.
+        """
         self.cookie = requests.get(self.cookies_endpoint).cookies
         print('New cookie created!')
-    
-    def get_countries(self):
-        self.countries = requests.get(self.country_endpoint, cookies = self.cookie)
-        print(f"The retrieved countries are: {self.countries.text}")
 
-    def get_leaders(self, cntry) -> None:
-        self.leader_country_endpoint = self.leaders_endpoint + '?country=' + cntry
-        self.leader = requests.get(self.leader_country_endpoint, cookies=self.cookie)
-        self.dict_lst = ['id','first_name','last_name','birth_date','death_date','place_of_birth','wikipedia_url','start_mandate', 'end_mandate']
-        self.leader_json = self.leader.json()
-        for a in self.dict_lst:
-            self.leaders_data[a] = [self.leader_json[i][a] for i in range(len(self.leader_json))]
-        
+    def get_countries(self):
+        """
+        Retrieve and return information about available countries.
+
+        Returns:
+        - str: A string containing information about available countries.
+        """
+        self.countries = requests.get(self.country_endpoint, cookies=self.cookie)
+        while self.countries.status_code != 200:
+            self.refresh_cookie()
+        return self.countries.text
+
+    def get_leaders(self) -> None:
+        """
+        Retrieve information about leaders of specific countries and store it in the leaders_data dictionary.
+        """
+        clist = ["ma", "us", "fr", "be", "ru"]
+        self.leaders_data = []
+
+        for c in clist:
+            self.leader_country_endpoint = self.leaders_endpoint + '?country=' + c
+            self.leader = requests.get(self.leader_country_endpoint, cookies=self.cookie)
+            if self.countries.status_code != 200:
+                self.refresh_cookie()
+            self.dict_lst = ['id', 'first_name', 'last_name', 'birth_date', 'death_date', 'place_of_birth',
+                             'wikipedia_url', 'start_mandate', 'end_mandate']
+            self.leader_cntry = self.leader.json()
+
+            # Initialize an empty dictionary for each country
+            leaders_data_cntry = {}
+
+            for a in self.dict_lst:
+                # Check if key 'a' exists in the dictionary before accessing it
+                if self.leader_cntry and all(a in leader for leader in self.leader_cntry):
+                    leaders_data_cntry[a] = [leader[a] for leader in self.leader_cntry]
+                else:
+                    leaders_data_cntry[a] = []
+
+            self.leaders_data.append(leaders_data_cntry)
+
+        for country_data in self.leaders_data:
+            for url in country_data['wikipedia_url']:
+                country_data['biography'] = self.get_first_paragraph(url)
+
     def get_first_paragraph(self, wikipedia_url: str) -> str:
-        self.wikipedia_url = wikipedia_url  
+        """
+        Retrieve the first paragraph of a Wikipedia page given its URL.
+
+        Args:
+        - wikipedia_url (str): The URL of the Wikipedia page.
+
+        Returns:
+        - str: The first paragraph of the Wikipedia page.
+        """
+        self.wikipedia_url = wikipedia_url
         self.r = requests.get(wikipedia_url)
         self.soup = BeautifulSoup(self.r.text, 'html.parser')
-        
+
         for self.elem in self.soup.find_all('p'):
             if self.elem.find('b'):
                 self.paragraph = self.elem.text
                 break
-        
+
         regex_pattern = r'\[[^\]]*\]|\([^\)]*\)|[^\w\s,.\'-]'
         self.paragraph = re.sub(regex_pattern, "", self.paragraph)    # Remove all brackets and text between brackets
         self.paragraph = re.sub(r'\s{2,}', " ", self.paragraph)       # Reduce all multiple spaces to one space
         self.paragraph = re.sub(r'\s+([.,])', r'\1', self.paragraph)    # Remove spaces before points or commas
         return self.paragraph
-  
-    def append_wiki_bio(self):
-        bio = []
-        for url in self.leaders_data['wikipedia_url']:
-            self.paragraph = self.get_first_paragraph(url)
-            bio.append(self.paragraph)
-        self.leaders_data['biography'] = bio
 
-    def into_dataframe(self):
-        self.df = pd.DataFrame(self.leader_json)
+    def save_data(self, filetype, filename):
+        """
+        Save the scraped data to the specified file type.
+
+        Args:
+        - filetype (str): The type of file to save (json, excel, or csv).
+        - filename (str): The name of the file to save the data.
+
+        Raises:
+        - ValueError: If an invalid filetype is provided.
+        """
+        self.filetype = filetype
+        self.filename = filename
+        if self.filetype == "excel":
+            self.df.to_excel(self.filename)
+        elif self.filetype == "json":
+            self.to_json()
+            with open(f"{self.filename}.json", "w") as file:
+                json.dump(self.dumps, file)
+        elif self.filetype == 'csv':
+            with open(f"{self.filename}.csv", 'w', newline='', encoding='utf-8') as csv_file:
+                fieldnames = ['id', 'first_name', 'last_name', 'birth_date', 'death_date', 'place_of_birth',
+                               'wikipedia_url', 'start_mandate', 'end_mandate', 'biography']
+
+                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+                # Write the header
+                writer.writeheader()
+
+                # Write data rows
+                for leader_data in self.leaders_data:
+                    writer.writerow(leader_data)
+        else:
+            raise ValueError('Enter a valid filetype: json, excel, or csv')
 
     def to_json(self):
-            self.result = self.df.to_json(orient="index")
-            self.parsed = loads(self.result)
-            self.dumps = dumps(self.parsed, indent=4)  
+        """
+        Convert the scraped data to JSON format.
+        """
+        self.dumps = dumps(self.leaders_data, indent=2)
 
-    def save_data(self, filetype):
-            self.filetype = filetype
-            if self.filetype == "excel":
-                self.df.to_excel('leaders.xlsx')
-            elif self.filetype == "json":
-                self.to_json()
-                with open("leaders.json", "w") as file:
-                    json.dump(self.dumps, file)
-            elif self.filetype == 'csv':
-                with open('dict.csv', 'w') as csv_file:  
-                    writer = csv.writer(csv_file)
-                    for key, value in mydict.items():
-                        writer.writerow([key, value])
-            else:
-                print('Enter a valid filetype, json or excel')
-    
     def __repr__(self) -> str:
         return "This is a WikipediaScraper object."
-
-wikiscrap = WikipediaScraper()
-wikiscrap.get_leaders('ru')
